957e36c438bcb175391e7aa0f5f850f03465d589
ilaytzu <ilay.tzu@gmail.com>
2016-10-03 23:55:44 +0300

    tested the start of classes

--- test.rb
+++ test.rb
@@ -1,2 +1,12 @@
-print "hello world"
-next=gets.chomp 
+class ALLA
+    def initialize(x)
+        @x=x
+    end
+    def get_twice
+        return (@x.to_i)*2
+    end
+end
+
+a = ALLA.new(15)
+puts a.get_twice
+gets
